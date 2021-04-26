5cd5171a028285f270473bd254c63418d1df8789
Bartek <submission93@gmail.com>
2021-04-26 19:24:09 +0200

    responsive home

--- src/Home.js
+++ src/Home.js
@@ -1,15 +1,26 @@
 import React, { useEffect } from "react";
 import image from "./images/IMG_2006.JPG";
+
 const Home = () => {
+  const openMenu = () => {
+    const menu = document.querySelector(".menu");
+    menu.classList.add("menu--is-visible");
+  };
+
+  const closeMenu = () => {
+    const menu = document.querySelector(".menu");
+    menu.classList.remove("menu--is-visible");
+  };
+
   return (
     <>
       <div className="container">
         <nav className="nav">
-          <span className="hamburger-icon">
+          <span onClick={openMenu} className="hamburger-icon">
             <i className="fas fa-bars"></i>
           </span>
 
-          <div className="menu">
+          <div onClick={closeMenu} className="menu">
             {" "}
             <ul className="nav-list">
               <li className="nav-item">Home</li>
@@ -19,14 +30,18 @@ const Home = () => {
           </div>
         </nav>
 
-        <div className="description">
-          <div className="image">
+        <div className="presentation">
+          <div className="main-description">
             <a className="img-link" target="_blank" href="https://github.com/flowbartek93">
               {" "}
               <img className="img" src={image} alt="" />
             </a>
+            <div className="text">
+              <strong className="text-1">{`<Welcome`}</strong>
+              <strong className="text-2">To</strong>
+              <strong className="text-3">My world{` />`}</strong>
+            </div>
           </div>
-          <div className="text"></div>
         </div>
       </div>
     </>
