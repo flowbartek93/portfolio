33733efdc80d158053d29f9056bece07ffbb6780
Bartek <submission93@gmail.com>
2021-04-27 13:54:31 +0200

    change color of <

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,7 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import image from "./images/IMG_2006.JPG";
-
+import { useImmer } from "use-immer";
+import Typewritter from "typewriter-effect";
 const Home = () => {
   const openMenu = () => {
     const menu = document.querySelector(".menu");
@@ -10,6 +11,15 @@ const Home = () => {
   const closeMenu = () => {
     const menu = document.querySelector(".menu");
     menu.classList.remove("menu--is-visible");
+  };
+
+  const createOpenTag = () => {
+    const span = document.createElement("span");
+    span.classList.add("tag-color");
+
+    span.textContent = "&lt;";
+
+    return span;
   };
 
   return (
@@ -34,12 +44,46 @@ const Home = () => {
           <div className="main-description">
             <a className="img-link" target="_blank" href="https://github.com/flowbartek93">
               {" "}
+              <span className="git-icon">
+                <i className="fab fa-github"></i>
+              </span>
               <img className="img" src={image} alt="" />
             </a>
             <div className="text">
-              <strong className="text-1">{`<Welcome`}</strong>
-              <strong className="text-2">To</strong>
-              <strong className="text-3">My world{` />`}</strong>
+              <strong className="text-1">
+                {}
+
+                <Typewritter
+                  options={{
+                    cursor: null
+                  }}
+                  onInit={typewritter => {
+                    typewritter.typeString(`${createOpenTag().outerHTML}Welcome`).start();
+                  }}
+                />
+              </strong>
+              <strong className="text-2">
+                {" "}
+                <Typewritter
+                  options={{
+                    cursor: null
+                  }}
+                  onInit={typewritter => {
+                    typewritter.typeString(`To`).start();
+                  }}
+                />
+              </strong>
+              <strong className="text-3">
+                {" "}
+                <Typewritter
+                  options={{
+                    cursor: null
+                  }}
+                  onInit={typewritter => {
+                    typewritter.typeString(`My world />`).start();
+                  }}
+                />
+              </strong>
             </div>
           </div>
         </div>
